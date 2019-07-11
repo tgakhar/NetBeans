2d615dece5ae3cfb39687e85713a7793875c815e
Tanvi Gakhar <gakhartanvi@gmail.com>
2019-07-11 14:39:14 -0400

    Initial commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author 1992591
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=10;
+        if(a%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+    
+}
